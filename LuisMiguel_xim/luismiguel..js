eb98fd6b39ccd78afcc00d4ce95eb1d41e5ade46
xim-sgr <ximenasalgadoramirez@gmail.com>
2025-06-27 19:38:51 -0600

    Add files via upload

---
+++ LuisMiguel_xim/luismiguel..js
@@ -0,0 +1,9 @@
+function irABiografia() {
+  document.getElementById("pantalla1").style.display = "none";
+  document.getElementById("pantalla2").style.display = "flex";
+}
+
+function irADiscos() {
+  document.getElementById("pantalla2").style.display = "none";
+  document.getElementById("pantalla3").style.display = "flex";
+}
